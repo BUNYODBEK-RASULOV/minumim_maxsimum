7f9ffa4a3cfeea871c916abb606059959aac7d7f
BUNYODBEK RASULOV <87300708+BUNYODBEK-RASULOV@users.noreply.github.com>
2021-08-02 09:29:07 +0500

    eng kichigi va eng kattasi

---
+++ minimum_va_maksimumga_oid_masalalar.py
@@ -0,0 +1,25 @@
+# 1-misol
+a=float(input("a sonni kiriting:"))
+b=float(input("b sonni kiriting:"))
+c=float(input("c sonni kiriting:"))
+d=float(input("d sonni kiriting:"))
+# 1-Usul
+# lirst=[a,b,c,d]
+# aa=a
+# for x in lirst:
+#     if aa<x:
+#         aa=x
+# print(aa)
+
+#2-usul
+# lirst=[a,b,c,d]
+# print(max(lirst))
+
+#3-usul
+# print(max(a,b,c,d))
+# lekin bizdan yuqoridagilarni hech biridan foydalanishga ruxsat yuq
+
+#4-usul
+
+
+
